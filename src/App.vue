067ee9fafdfac34dd6ca62bc9749359b85437101
Gwynshik <gwynshik@gmail.com>
2023-01-03 18:04:16 +0300

    add header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-
+	<Header />
+	<div class="footer"></div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  components:{
-}}
+  import Header from '@/components/Header.vue'
+  export default {
+    name: 'App',
+    components:{
+      Header,
+  }}
 </script>
 
 <style>
-
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html, body {
+    width: 100%;
+    height: 100%;
+  }	
+  .container{
+    width: 100%;
+    padding-left: 20%;
+    padding-right: 20%;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+        <div class="container">
+            <nav class="navbar">
+                <a href="" class="navbar__item logo"><img src="@/images/logo.svg" alt="ivi"></a>
+                <div class="navbar__item darkmode-switch">switch</div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<style>
+    .header{
+        height: 50px;
+        display: flex;
+        align-items: center;
+        background-color: #e0552a;
+    }
+    nav{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+
+    }
+</style>
