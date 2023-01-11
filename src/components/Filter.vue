8f65fd48a1dcdd846fe6e621b802337c3a7d66c0
Gwynshik <gwynshik@gmail.com>
2023-01-11 22:38:13 +0300

    Renamed classes in Filter.vue

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,29 +1,59 @@
 <template>
     <div class="filter">
         <div class="filter__container">
-            <div class="filter__inner-container genre">
-                <div class="filter__select-genre" @click="changeDisplayFilterDropdown">
+            <div class="filter__inner-container genres">
+                <div class="select-genres" @click="changeDisplayDropdownMenuGenres">
+                    <div class="select-genres__text-wrapper">
+                        <span class="select-genres__title">Жанры</span>
+                        <span class="select-genres__text">{{ selectedGenresText }}</span>
+                    </div>
+                    <div class="select-genres__icon">v</div>
+                </div>
+                <div class="dropdown-menu-genres" :class="{'dropdown-menu-genres_active': dropdownMenuGenresIsActive}">
+                    <!-- <div class="dropdown-menu-genres__slider">
+                        <div class="dropdown-menu-genres__slider-item">1</div>
+                        <div class="dropdown-menu-genres__slider-item">2</div>
+                        <div class="dropdown-menu-genres__slider-item">3</div>
+                        <div class="dropdown-menu-genres__slider-item">4</div>
+                    </div> -->
+                    <ul class="dropdown-menu-genres__list">
+                        <li v-for="genre of genres" class="dropdown-menu-genres__item" >
+                            <label for="checkbox-genre" class="dropdown-menu-genres__label" >
+                                <input type="checkbox" id="checkbox-genre" style="display: none">
+                                <div class="dropdown-menu-genres__text" @click="selectGenre(genre)">{{ genre.title }}</div>
+                                <div class="dropdown-menu-genres__checkbox-wrapper">
+                                    <div class="dropdown-menu-genres__checkbox">
+                                        <img src="@/images/check-icon.svg" alt="checkbox" class="dropdown-menu-genres__icon">
+                                    </div>
+                                </div>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="filter__inner-container country">
+                <div class="filter__select-country" @click="changeDisplayDropdownMenuCountry">
                     <div class="filter__text-wrapper">
-                        <span class="filter__title">Жанры</span>
-                        <span class="filter__select-genre-text">{{ selectedGenresText }}</span>
+                        <span class="filter__title">Страна</span>
+                        <span class="filter__select-country-text">{{ selectedCountryText }}</span>
                     </div>
                     <div class="filter__icon">v</div>
                 </div>
-                <div class="filter-dropdown" :class="{'filter-dropdown_active': FilterDropdownIsActive}">
+                <div class="dropdown-menu-country" style="display: none" :class="{'dropdown-menu-country_active': dropdownMenuCountryIsActive}">
                     <!-- <div class="filter__dropdown-slider">
                         <div class="filter__slider-item">1</div>
                         <div class="filter__slider-item">2</div>
                         <div class="filter__slider-item">3</div>
                         <div class="filter__slider-item">4</div>
                     </div> -->
-                    <ul class="filter-dropdown__list">
-                        <li v-for="genre of genres" class="filter-dropdown__item" >
-                            <label for="checkbox-genre" class="filter-dropdown__label" >
+                    <ul class="dropdown-menu-country__list">
+                        <li v-for="genre of genres" class="dropdown-menu-country__item" >
+                            <label for="checkbox-genre" class="dropdown-menu-country__label" >
                                 <input type="checkbox" id="checkbox-genre" style="display: none">
-                                <div class="filter-dropdown__text" @click="selectGenre(genre)">{{ genre.title }}</div>
-                                <div class="filter-dropdown__checkbox-wrapper">
-                                    <div class="filter-dropdown__checkbox">
-                                        <img src="@/images/check-icon.svg" alt="checkbox" class="filter-dropdown__icon">
+                                <div class="dropdown-menu-country__text" @click="selectCountry(country)">{{ country.title }}</div>
+                                <div class="dropdown-menu-country__checkbox-wrapper">
+                                    <div class="dropdown-menu-country__checkbox">
+                                        <img src="@/images/check-icon.svg" alt="checkbox" class="dropdown-menu-country__icon">
                                     </div>
                                 </div>
                             </label>
@@ -72,8 +102,46 @@
                     {id:25,title:"Фантастика",selected:false},
                     {id:26,title:"Фэнтези",selected:false}
                 ],
+                country: [
+                    {id:1,title:"Австралия",selected:false},
+                    {id:2,title:"Аргентина",selected:false},
+                    {id:3,title:"Армения",selected:false},
+                    {id:4,title:"Беларусь",selected:false},
+                    {id:5,title:"Бельгия",selected:false},
+                    {id:5,title:"Бразилия",selected:false},
+                    {id:6,title:"Великобритания",selected:false},
+                    {id:7,title:"Венгрия",selected:false},
+                    {id:8,title:"Германия",selected:false},
+                    {id:9,title:"Гонконг",selected:false},
+                    {id:10,title:"Дания",selected:false},
+                    {id:11,title:"Индия",selected:false},
+                    {id:12,title:"Ирландия",selected:false},
+                    {id:13,title:"Испания",selected:false},
+                    {id:14,title:"Италия",selected:false},
+                    {id:15,title:"Казахстан",selected:false},
+                    {id:16,title:"Канада",selected:false},
+                    {id:17,title:"Китай",selected:false},
+                    {id:18,title:"Колумбия",selected:false},
+                    {id:19,title:"Мексика",selected:false},
+                    {id:20,title:"Новая Зеландия",selected:false},
+                    {id:21,title:"Норвегия",selected:false},
+                    {id:23,title:"Польша",selected:false},
+                    {id:24,title:"Россия",selected:false},
+                    {id:25,title:"СССР",selected:false},
+                    {id:26,title:"США",selected:false},
+                    {id:27,title:"Таиланд",selected:false},
+                    {id:28,title:"Турция",selected:false},
+                    {id:29,title:"Украина",selected:false},
+                    {id:30,title:"Финляндия",selected:false},
+                    {id:31,title:"Франция",selected:false},
+                    {id:32,title:"Швейцария",selected:false},
+                    {id:33,title:"Швеция",selected:false},
+                    {id:34,title:"ЮАР",selected:false},
+                    {id:35,title:"Южная Корея",selected:false},
+                    {id:36,title:"Япония",selected:false},
+                ],
                 countClick: 0,
-                FilterDropdownIsActive: false,
+                dropdownMenuGenresIsActive: false,
             }
         },
         computed:{
@@ -88,8 +156,8 @@
             }
         },
         methods: {
-            changeDisplayFilterDropdown(){
-                this.FilterDropdownIsActive = !this.FilterDropdownIsActive
+            changeDisplayDropdownMenuGenres(){
+                this.dropdownMenuGenresIsActive = !this.dropdownMenuGenresIsActive
             },
             selectGenre(genre){
                 genre.selected = !genre.selected;
@@ -129,7 +197,7 @@
         flex-direction: column;
         gap: 10px;
     }
-    .filter__select-genre{
+    .select-genres{
         width: 100%;
         display: flex;
         justify-content: space-between;
@@ -139,11 +207,11 @@
         border-radius: 5px;
         cursor: pointer;
     }
-    .filter__text-wrapper{
+    .select-genres__text-wrapper{
         display: flex;
         flex-direction: column;
     }
-    .filter-dropdown{
+    .dropdown-menu-genres{
         overflow: hidden;
         width: 630px;
         background-color: #e76a44;
@@ -152,41 +220,41 @@
         border-radius: 5px;
         display: none;
     }
-    .filter-dropdown_active{
+    .dropdown-menu-genres_active{
         display: block;
     }
-    .filter-dropdown_slider{
+    .dropdown-menu-genres_slider{
         display: flex;
     }
-    .filter-dropdown__list{
+    .dropdown-menu-genres__list{
         display: flex;
         flex-wrap: wrap;
         flex-direction: revert;
         justify-content: space-between;
         gap: 7px;
     }
-    .filter-dropdown__item{
+    .dropdown-menu-genres__item{
         position: relative;
         width: 30%;
         list-style: none;
     }
-    .filter-dropdown__label{
+    .dropdown-menu-genres__label{
         display: flex;
         cursor: pointer;
         align-items: center;
     }
-    .filter-dropdown__label:hover .filter-dropdown__icon{
+    .dropdown-menu-genres__label:hover .dropdown-menu-genres__icon{
         opacity: 0.5;
     }
-    .filter-dropdown__text{
+    .dropdown-menu-genres__text{
         width: 180px;
         overflow: hidden;
     }
-    .filter-dropdown__icon{
+    .dropdown-menu-genres__icon{
         width: 24px;
         opacity: 0;
     }
-    .filter-dropdown__icon:active{
+    .dropdown-menu-genres__icon:active{
         opacity: 1;
     }
     .filter__btn-reset{
